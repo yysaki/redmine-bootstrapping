5d305ae84f0dce83b72e85b1ed122b469e2c348e
tamakoron <tamakoron7@gmail.com>
2016-08-12 06:25:39 +0900

    disable interactive configure by 'apt-get upgrade'

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
 #general #{{{1
-
+echo "set grub-pc/install_devices /dev/sda" | sudo debconf-communicate
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y git openssh-server language-pack-ja
