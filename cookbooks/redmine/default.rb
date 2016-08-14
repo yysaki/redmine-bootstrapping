18a0a7252348859e11eaee0dd1467bf4281d314b
yysaki <yysaki@mail.yysaki.com>
2016-08-14 16:33:49 +0900

    Update redmine cookbooks to activate redmine app

--- cookbooks/redmine/default.rb
+++ cookbooks/redmine/default.rb
@@ -38,3 +38,106 @@ package "nginx"
 execute "gem install bundler" do
   command "gem install bundler"
 end
+
+user "create unicorn user" do
+  username "unicorn"
+  password ""
+  home "/home/unicorn"
+  shell	"/bin/bash"
+end
+
+directory "/home/unicorn" do
+  owner "unicorn"
+  group "unicorn"
+end
+
+git "clone redmine" do
+  user "unicorn"
+  destination "/home/unicorn/redmine"
+  repository "https://github.com/redmine/redmine.git"
+  revision "3.2.3"
+end
+
+remote_file "/home/unicorn/redmine/config/database.yml" do
+  owner "unicorn"
+  group "unicorn"
+end
+
+remote_file "/home/unicorn/redmine/Gemfile.local" do
+  owner "unicorn"
+  group "unicorn"
+end
+
+remote_file "/home/unicorn/redmine/config/unicorn.rb" do
+  owner "unicorn"
+  group "unicorn"
+end
+
+execute "bundle install" do
+  user "unicorn"
+  cwd "/home/unicorn/redmine"
+  command "bundle install --path .bundle --without development test"
+end
+
+execute "bundle update" do
+  user "unicorn"
+  cwd "/home/unicorn/redmine"
+  command "bundle update"
+end
+
+execute "redmine rake generate_secret_token" do
+  user "unicorn"
+  cwd "/home/unicorn/redmine"
+  command "bundle exec rake generate_secret_token"
+end
+
+execute "redmina db migration" do
+  cwd "/home/unicorn/redmine"
+  command "RAILS_ENV=production bundle exec rake db:migrate"
+end
+
+execute "redmine load_default_data" do
+  cwd "/home/unicorn/redmine"
+  command "RAILS_ENV=production REDMINE_LANG=ja bundle exec rake redmine:load_default_data"
+end
+
+execute "chmod tmp/" do
+  command "chmod 777 /home/unicorn/redmine/tmp"
+end
+
+execute "chmod db/" do
+  command "chmod 777 /home/unicorn/redmine/db"
+end
+
+execute "chmod redmine.db" do
+  command "chmod 777 /home/unicorn/redmine/db/redmine.db"
+end
+
+remote_file "/lib/systemd/system/redmine-unicorn.service"
+
+service "redmine-unicorn" do
+  action :restart
+end
+
+service "redmine-unicorn" do
+  action :enable
+end
+
+remote_file "/etc/nginx/sites-available/redmine"
+
+link "/etc/nginx/sites-enabled/redmine" do
+  to "/etc/nginx/sites-available/redmine"
+end
+
+file "delete nginx site-enabled/default" do
+  action :delete
+  path "/etc/nginx/sites-enabled/default"
+end
+
+service "nginx" do
+  action :restart
+end
+
+service "nginx" do
+  action :enable
+end
---
+++ cookbooks/redmine/files/home/unicorn/redmine/config/unicorn.rb
@@ -0,0 +1,43 @@
+worker_processes 1
+
+listen "/home/unicorn/redmine/tmp/unicorn_redmine.sock", :backlog => 32
+pid "tmp/pids/unicorn.pid"
+
+stderr_path 'log/unicorni_error.log'
+stdout_path 'log/unicorn.log'
+
+preload_app true
+check_client_connection false
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  begin
+    uid, gid = Process.euid, Process.egid
+    user, group = "unicorn", "unicorn"
+    ENV["HOME"] = "/home/unicorn/redmine"
+    target_uid = Etc.getpwnam(user).uid
+    target_gid = Etc.getgrnam(group).gid
+    worker.tmp.chown(target_uid, target_gid)
+    if uid != target_uid or gid != target.gid
+      Process.initgroups(user, target_gid)
+      Process::GID.change_privilege(target_gid)
+      Process::UID.change_privilege(target_uid)
+    end
+  rescue
+    if RAILS_ENV = "development"
+      STDERR.puts "could not change user, oh well"
+    else
+      STDERR.puts "could not change user, oh well"
+      raise e
+    end
+  end
+end
+
